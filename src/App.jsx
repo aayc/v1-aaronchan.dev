b0477e7df97dc54ec161ce78d2b45ae05d4b417b
Aaron Chan <aaron.y.chan64@gmail.com>
2019-06-01 21:56:46 -0600

    added relatively positioned words to main page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Avatar from '@material-ui/core/Avatar';
 import { Link, Switch, Route, BrowserRouter }  from 'react-router-dom';
 import Math495Home from './math495/Math495Home.jsx';
 import Projects from './Projects.jsx';
+import Fade from '@material-ui/core/Fade';
 import Button from '@material-ui/core/Button';
 import ContactButton from './Contact.jsx';
 import Grid from '@material-ui/core/Grid';
@@ -38,18 +39,24 @@ class App extends Component {
 
 class DefaultContainer extends Component {
   render() {
+
     return (
         <div className="App">
           <Grid container spacing={0} style={{ /*background: "#336633",*/ }}>
             <Grid item xs={12} sm={12} style={{ marginTop: "8%" }}> 
               <center>
-                <Avatar alt="Aaron Chan" src="me-close.jpg" style={{width: 300, height: 300 }}/>
-                <br />
+                <div style={{ position: "relative" }}>
+                  <Avatar alt="Aaron Chan" src="me-close.jpg" style={{width: 300, height: 300 }}/>
+                  <FloatText len={4000} mTop="20px" mLeft="180px" text="urbanite" />
+                  <FloatText len={1000} mTop="140px" mLeft="200px" text="coder" />
+                  <FloatText len={2000} mTop="260px" mLeft="180px" text="pianist" />
+                  <FloatText len={2500} mTop="20px" mLeft="-200px" text="statistician" />
+                  <FloatText len={1500} mTop="140px" mLeft="-200px" text="chef" />
+                  <FloatText len={3000} mTop="260px" mLeft="-190px" text="friend" />
+                </div> <br /><br />
                 <Typography component="h1" variant="h2">
                   Hi, I'm Aaron Chan.
                 </Typography>
-        {/* urbanite friend coder pianist chef engineer cellist clown */}
-      {/*<Typography variant="overline" style={{ position: "absolute", top: "20%" }} >Hello</Typography>*/}
                 <br /> <br />
                 <Link to="/projects" style={cssStyles.buttonLink}>
                   <Button variant="outlined" style={{ margin: 10, fontWeight: "bold"}}>
@@ -67,6 +74,25 @@ class DefaultContainer extends Component {
           </Grid>
         </div>
       )
+  }
+}
+
+class FloatText extends Component {
+  render () {
+    const { mTop, mLeft, text, len } = this.props
+    return ( 
+      <Fade in={true} timeout={len}>
+        <div>
+        <Typography style = {{ 
+          position: "absolute", 
+          width: "100%",
+          textAlign: "center",
+          left: mLeft,
+          top: mTop,
+        }} variant="overline">{text}</Typography>
+        </div>
+      </Fade>
+    )
   }
 }
 
