f4c4e7ec7d90fffd82a21030917a9bb9042284f6
Aaron Chan <aaron.y.chan64@gmail.com>
2019-09-05 15:25:07 -0600

    updated with slides and problem set

--- src/math495/Course.jsx
+++ src/math495/Course.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Code from './PythonCodeBlock.jsx';
 
 class Course extends Component {
   render () {
@@ -10,8 +9,12 @@ class Course extends Component {
       <h1>Course Materials</h1>
       <h3>Course Slides</h3>
         <a href="https://docs.google.com/presentation/d/1dnLAHWUWIjsXN5lN6KxpmIMUnLMjYevEPayIyigEo9w/edit?usp=sharing" target="_blank">Day 1: Syllabus and Slides</a>
+        <br/>
+        <a href="https://docs.google.com/presentation/d/1_wDI0PU3jm5vl5DDPKYlCY7kqkERC0uU1mO3H7_IJx4/edit?usp=sharing" target="_blank">Day 2: Common Data Structures</a>
       <h3>Problem Sets</h3>
         <a href="http://www.hackerrank.com/python-practice-1566475321" target="_blank">Problem Set 1: Python Practice</a>
+        <br />
+        <a href="http://www.hackerrank.com/math-495r-09-data-structures" target="_blank">Problem Set 2: Data Structures</a>
       </div>
     )
   }
