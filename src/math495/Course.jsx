d4c65a0451cca2fb5fab5accd8602770d39ec5a5
Aaron Chan <aaron.y.chan64@gmail.com>
2019-09-17 13:15:44 -0600

    upd course materials

--- src/math495/Course.jsx
+++ src/math495/Course.jsx
@@ -13,12 +13,16 @@ class Course extends Component {
         <a href="https://docs.google.com/presentation/d/1_wDI0PU3jm5vl5DDPKYlCY7kqkERC0uU1mO3H7_IJx4/edit?usp=sharing" target="_blank">Day 2: Common Data Structures</a>
         <br/>
         <a href="https://docs.google.com/presentation/d/1Shoeu39jQSdRw8boOOWbofw74VcqXPPvnzMZbsePj7M/edit?usp=sharing" target="_blank">Day 3: Review of Data Structures + Heap + Trie</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/1034NwGpbmRc4T0pLiGOFaxN9vzMrWwJp4H6jl_H4n_8/edit?usp=sharing" target="_blank">Day 5: Strings</a>
       <h3>Problem Sets</h3>
         <a href="http://www.hackerrank.com/python-practice-1566475321" target="_blank">Problem Set 1: Python Practice</a>
         <br />
         <a href="http://www.hackerrank.com/math-495r-09-data-structures" target="_blank">Problem Set 2: Data Structures</a>
         <br />
         <a href="http://www.hackerrank.com/math-495r-09-strings" target="_blank">Problem Set 3: Strings</a>
+        <br />
+        <a href="http://www.hackerrank.com/math-495r-09-brute-force" target="_blank">Problem Set 4: Brute Force</a>
       </div>
     )
   }
