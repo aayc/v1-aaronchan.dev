3ab821081a746e7d9f966ece870f9af554a4644a
Aaron Chan <aaron.y.chan64@gmail.com>
2019-11-07 10:41:11 -0700

    upd course materials

--- src/math495/Course.jsx
+++ src/math495/Course.jsx
@@ -23,6 +23,20 @@ class Course extends Component {
         <a href="https://docs.google.com/presentation/d/1vnMHejYCVtl-x20mMFjqb9N3YhKdVCFMkA7ZVeyG4bY/edit?usp=sharing" target="_blank">Day 8: Brute Force</a>
         <br />
         <a href="https://docs.google.com/presentation/d/1_Q-Z6eo63dv-fBzbZMSZ8YRNIQSPIQSRYiLBP6CHGX0/edit?usp=sharing" target="_blank">Day 11: Recursion</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/11p11PvLfv2JsLBdFYtqAg9d1LYS9EkyHm5j3bNnCU6E/edit?usp=sharing" target="_blank">Day 12: Memoziation</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/1qY1RNzQKCXC03jGvOIWt6JuteFPPpgCVwBQ5MHmq1Vg/edit?usp=sharing" target="_blank">Day 14: Graph Algorithms I</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/14p2dxh77KnDZkoLibwp3PN5-WAtqYiCu9D3Xve9S6cU/edit?usp=sharing" target="_blank">Day 15: Graph Algorithms II</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/1-h_8eSk2ha6JCEweAEyR1zyuf0Rx9WuQ0V-LRoIePvE/edit?usp=sharing" target="_blank">Day 16: Graph Algorithms III</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/1rZ-Adv6tFMGYe774kKQKSToqJK6Os0FYFPTsD64UuiA/edit?usp=sharing" target="_blank">Day 17: Dynamic Programming</a>
+        <br />
+        <a href="https://docs.google.com/presentation/d/13UJzt_7bxUY9XEsg66p9W-5Xa7gEoS1oCCG5v842cB4/edit?usp=sharing" target="_blank">Day 18: Dynamic Programming II</a>
+
+
       <h3>Problem Sets</h3>
         <a href="http://www.hackerrank.com/python-practice-1566475321" target="_blank">Problem Set 1: Python Practice</a>
         <br />
@@ -39,6 +53,8 @@ class Course extends Component {
         <a href="http://www.hackerrank.com/math-495r-09-recursion" target="_blank">Problem Set 6: Recursion</a>
         <br />
         <a href="http://www.hackerrank.com/math-495r-09-graph" target="_blank">Problem Set 7: Graph</a>
+        <br />
+        <a href="http://www.hackerrank.com/math-495r-09-dynamic-programming" target="_blank">Problem Set 8: Dynamic Programming</a>
       </div>
     )
   }
