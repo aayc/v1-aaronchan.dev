ee25a8031ce544453f5a7284da184bdf625348be
Aaron Chan <aaron.y.chan64@gmail.com>
2019-11-09 16:24:16 -0700

    added roboto typeface

--- src/blog/GPT2Blog.jsx
+++ src/blog/GPT2Blog.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
 
 const styles = theme => ({
   root: {
